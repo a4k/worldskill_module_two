e69f632472d977101d726045abd4b53e7d57b590
n.brovkovich <n.brovkovich@rsit.ru>
2019-11-04 21:17:12 +0400

    added elf

--- assets/js/app.js
+++ assets/js/app.js
@@ -115,6 +115,9 @@ function Game(options) {
                 _this.drawObject(v)
             })
 
+            let playerPerson = mediator.callTrigger(TRIGGER.GET_PLAYER_PERSON);
+            $('.kills-value').text(playerPerson.kills);
+
             setTimeout(_this.updateScene.bind(_this), 50)
         }
     }
@@ -139,6 +142,9 @@ function Game(options) {
             nextAnimation();
             animation.dwidth = animation.width / (animation.height / animation.dheight);
             let dy = settings.canvasHeight - animation.dheight;
+            if(current.hasOwnProperty('top')) {
+                dy -= current.top;
+            }
 
             let cleft = current.left;
 
@@ -256,6 +262,7 @@ function Player(options) {
         mp: 100,
         maxHp: 100,
         maxMp: 100,
+        kills: 0,
     }
     let animation = {
         idle: {
@@ -371,12 +378,33 @@ function Player(options) {
                     settings.skill_active = false;
                     $('.attack1').removeClass('disable');
                     _this.setAnimation('idle');
+
+                    let enemies = mediator.callTrigger(TRIGGER.GET_ENEMIES);
+                    let canvas = mediator.callTrigger(TRIGGER.GET_CANVAS_SETTINGS);
+
+                    $.each(enemies, function (k, v) {
+                        if(v) {
+                            let enemy = v.getAnimation();
+                            let myLeft = animation.current.left;
+                            let enemyLeft = enemy.current.left - canvas.canvasLeft;
+                            if(animation.current.direction === NAMES.DIRECTION_RIGHT) {
+                                if(enemyLeft < myLeft + 600 && enemyLeft > myLeft - 200) {
+                                    v.damage(_this.getPerson());
+                                }
+                            } else {
+                                if(enemyLeft > myLeft - 600 && enemyLeft < myLeft + 200) {
+                                    v.damage(_this.getPerson());
+                                }
+                            }
+                        }
+                    })
                 }
             };
             _this.setAnimation('attack1', cb);
             $('.attack1').addClass('disable');
 
             settings.skill_active = true;
+
         }
     }
     this.damage = (data) => {
@@ -386,6 +414,9 @@ function Player(options) {
         let hp = $('.panel-xp');
         hp.find('span').text(person.hp);
         hp.find('.score-value').css({width: percent + '%'});
+        if(person.hp <= 0) {
+            mediator.callTrigger(TRIGGER.END_GAME, {})
+        }
     }
 
     function init() {
@@ -397,6 +428,7 @@ function Player(options) {
         mediator.subscribe(TRIGGER.PLAYER_IDLE, _this.idle.bind(this))
         mediator.subscribe(TRIGGER.PLAYER_DAMAGE, _this.damage.bind(this))
         mediator.subscribe(TRIGGER.GET_PLAYER_ANIMATION, _this.getAnimation.bind(this))
+        mediator.subscribe(TRIGGER.GET_PLAYER_PERSON, _this.getPerson.bind(this))
     }
     init();
 }
@@ -414,14 +446,21 @@ function EnemyManager(options) {
         purpose: {},
         maxObjects: 0,
         maxEnemyLevel: 0,
+        maxSceneObjects: 10,
     };
 
-    this.updateEnemyLevel = (data = {level: 0}) => {
-        if(data.level < 2) {
+    this.updateEnemyLevel = (data) => {
+        let player = mediator.callTrigger(TRIGGER.GET_PLAYER_PERSON);
+        let level = player.kills;
+
+        if(level < 2) {
             settings.maxObjects = 1;
             settings.maxEnemyLevel = 1;
         } else {
-            settings.maxObjects = settings.maxObjects * data.level;
+            settings.maxObjects = level;
+            if(settings.maxObjects > settings.maxSceneObjects) {
+                settings.maxObjects = settings.maxSceneObjects;
+            }
             if(settings.maxObjects > 5) {
                 settings.maxEnemyLevel = 3;
             } else if(settings.maxObjects > 3) {
@@ -456,6 +495,8 @@ function EnemyManager(options) {
             switch (randLevel) {
                 case 1:
                     this.addObject(new Dog({mediator}));
+                case 2:
+                    this.addObject(new Elf({mediator}));
             }
         }
     }
@@ -472,12 +513,24 @@ function EnemyManager(options) {
 
     this.addObject = (obj) => {
         let canvas = mediator.callTrigger(TRIGGER.GET_CANVAS_SETTINGS);
-        obj.setPosition(canvas.canvasWidth - 300);
+        let x = Math.floor(Math.random() * 400) + 1;
+        obj.setPosition(canvas.canvasLeft + canvas.canvasWidth + x);
         let a = obj.getAnimation();
         objects.push(obj);
     }
     this.getObjects = () => {
         return objects;
+    }
+    this.destroyEnemy = (data) => {
+        $.each(objects, function (k, v) {
+            if(v) {
+                let p = v.getPerson();
+                if(p.id === data.id) {
+                    objects.splice(k, 1);
+                }
+            }
+        })
+        this.updateEnemyLevel();
     }
 
 
@@ -485,6 +538,7 @@ function EnemyManager(options) {
         mediator.subscribe(TRIGGER.UPDATE_ENEMY_LEVEL, _this.updateEnemyLevel.bind(this))
         mediator.subscribe(TRIGGER.GET_ENEMIES, _this.getObjects.bind(this))
         mediator.subscribe(TRIGGER.UPDATE_PURPOSE, _this.updatePurpose.bind(this))
+        mediator.subscribe(TRIGGER.DESTROY_ENEMY, _this.destroyEnemy.bind(this))
 
         _this.updateEnemyLevel()
         _this.render();
@@ -500,6 +554,7 @@ function Dog(options) {
     const _this = this;
 
     let person = {
+        id: 0,
         damage: 2,
         hp: 15,
         maxHp: 15,
@@ -519,7 +574,7 @@ function Dog(options) {
             name: 'run',
             number: 1,
             left: 0,
-            sleft: 0,
+            top: 0,
             direction: NAMES.DIRECTION_RIGHT,
             callback: false,
         }
@@ -597,6 +652,7 @@ function Dog(options) {
         } else if(purposeLeft < c.left) {
             this.moveLeft(-0.5);
         }
+        this.setAnimation('run')
     }
 
     this.purposeDamage = () => {
@@ -609,11 +665,205 @@ function Dog(options) {
         mediator.callTrigger(TRIGGER.PLAYER_DAMAGE, _this.getPerson());
     }
 
+    this.damage = (data) => {
+        person.hp -= data.damage;
+        if(person.hp <= 0) {
+            data.kills++;
+            mediator.callTrigger(TRIGGER.DESTROY_ENEMY, _this.getPerson());
+        }
+    }
+
     function init() {
 
+        person.id = Math.floor(Math.random() * 100000) + 1;
+        animation.current.top = Math.floor(Math.random() * 20) + 1;
         mediator.subscribe(TRIGGER.DOG_MOVE_LEFT, _this.moveLeft.bind(this))
         mediator.subscribe(TRIGGER.DOG_MOVE_RIGHT, _this.moveRight.bind(this))
         mediator.subscribe(TRIGGER.DOG_RUN, _this.run.bind(this))
+    }
+    init();
+}
+
+function Elf(options) {
+
+    const mediator = options.mediator;
+    const TRIGGER = mediator.getTriggerTypes();
+    const NAMES = mediator.getNames();
+    const _this = this;
+
+    let person = {
+        id: 0,
+        damage: 5,
+        hp: 30,
+        maxHp: 30,
+        canDamage: true,
+    };
+    let animation = {
+        idle: {
+            url: '/animation/elf/idle/',
+            postfix: '.png',
+            count: 20,
+            width: 341,
+            height: 778,
+            dheight: 350,
+            dwidth: 0,
+        },
+        run: {
+            url: '/animation/elf/run/',
+            postfix: '.png',
+            count: 20,
+            width: 592,
+            height: 794,
+            dheight: 350,
+            dwidth: 0,
+        },
+        die: {
+            url: '/animation/elf/die/',
+            postfix: '.png',
+            count: 20,
+            width: 1256,
+            height: 855,
+            dheight: 350,
+            dwidth: 0,
+        },
+        attack: {
+            url: '/animation/elf/attack/',
+            postfix: '.png',
+            count: 20,
+            width: 490,
+            height: 797,
+            dheight: 350,
+            dwidth: 0,
+        },
+        current: {
+            name: 'idle',
+            number: 1,
+            left: 0,
+            top: 0,
+            direction: NAMES.DIRECTION_RIGHT,
+            callback: false,
+        }
+    }
+
+    this.getAnimation = () => {
+        return animation;
+    }
+    this.getPerson = () => {
+        return person;
+    }
+    this.isSceneObject = () => {
+        return true;
+    }
+    this.moveLeft = (data) => {
+        if(data === 1) {
+            _this.move(-10, NAMES.DIRECTION_LEFT)
+        } else {
+            _this.move(data, NAMES.DIRECTION_LEFT)
+        }
+    }
+    this.moveRight = (data) => {
+        if(data === 1) {
+            _this.move(10, NAMES.DIRECTION_RIGHT)
+        } else {
+            _this.move(data, NAMES.DIRECTION_RIGHT)
+        }
+    }
+    this.setPosition = (data, direction = false) => {
+        let c = animation.current;
+        c.left = data;
+        if(direction) {
+            c.direction = direction;
+        }
+        _this.setAnimation('idle')
+    }
+    this.move = (data, direction = false) => {
+        let c = animation.current;
+
+        c.left += data;
+        if(direction) {
+            c.direction = direction;
+        }
+        _this.setAnimation('run')
+    }
+    this.run = (data) => {
+        _this.setAnimation('run')
+    }
+    this.idle = (data) => {
+        _this.setAnimation('idle')
+    }
+    this.setAnimation = (name, cb = false) => {
+
+        if(animation.current.name === name) {
+            return;
+        }
+
+        let c = animation.current;
+        c.name = name;
+        c.number = 1;
+        c.callback = cb;
+    }
+
+    this.moveToPurpose = (data) => {
+        let canvas = mediator.callTrigger(TRIGGER.GET_CANVAS_SETTINGS);
+        let cleft = canvas.canvasLeft;
+        if (!cleft) cleft = 0;
+
+        let purposeLeft = cleft + data.x;
+
+        let c = animation.current;
+        if(c.left < purposeLeft + 200 && c.left > purposeLeft - 50) {
+            this.purposeDamage();
+            return;
+        }
+        if(purposeLeft > c.left) {
+            this.moveRight(0.5);
+        } else if(purposeLeft < c.left) {
+            this.moveLeft(-0.5);
+        }
+    }
+
+    this.purposeDamage = () => {
+        if(!person.canDamage) return;
+
+        let cb = {
+            done: function() {
+                if(!person.canDamage) return;
+
+                _this.setAnimation('idle')
+            }
+        };
+        this.setAnimation('attack', cb)
+
+        person.canDamage = false;
+        setTimeout(() => {
+            person.canDamage = true;
+        }, 1000);
+        mediator.callTrigger(TRIGGER.PLAYER_DAMAGE, _this.getPerson());
+    }
+
+    this.damage = (data) => {
+        person.hp -= data.damage;
+        if(person.hp <= 0) {
+            person.canDamage = false;
+            data.kills++;
+            let cb = {
+                done: function() {
+                    mediator.callTrigger(TRIGGER.DESTROY_ENEMY, _this.getPerson());
+                }
+            };
+            _this.setAnimation('die', cb)
+        }
+    }
+
+    function init() {
+
+        person.id = Math.floor(Math.random() * 100000) + 1;
+        animation.current.top = Math.floor(Math.random() * 20) + 1;
+
+        mediator.subscribe(TRIGGER.ELF_MOVE_LEFT, _this.moveLeft.bind(this))
+        mediator.subscribe(TRIGGER.ELF_MOVE_RIGHT, _this.moveRight.bind(this))
+        mediator.subscribe(TRIGGER.ELF_RUN, _this.run.bind(this))
+        mediator.subscribe(TRIGGER.ELF_IDLE, _this.idle.bind(this))
     }
     init();
 }
